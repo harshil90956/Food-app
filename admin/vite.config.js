07ae9b80ecb79610aef413d0fa1d79109af88706
Harshil Dobariya <143210326+harshil90956@users.noreply.github.com>
2024-11-25 21:56:00 +0530

    Resolved merge conflicts in vite.config.js

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -6,8 +6,4 @@ export default {
     host: '0.0.0.0',
     port: process.env.PORT || 4174, // Render dynamically assigns the port
   },
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 3f31545923e748d4310b887d347a6eddb05608e1
