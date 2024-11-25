935cee0de2985ae2d5d602a0319a77d2a9cb8d33
Harshil Dobariya <143210326+harshil90956@users.noreply.github.com>
2024-11-25 21:18:44 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
-// vite.config.js
+import react from '@vitejs/plugin-react';
+
 export default {
+  plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 4173 // Render dynamically assigns the port
-  }
-}
+    port: process.env.PORT || 4173, // Render dynamically assigns the port
+  },
+};
